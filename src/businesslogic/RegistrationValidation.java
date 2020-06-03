5045c940e86988e959125e3efab2fddb37328557
krishnamohan <krishnamohan.py@gmail.com>
2020-06-03 10:15:25 +0530

    completed

--- src/businesslogic/RegistrationValidation.java
+++ src/businesslogic/RegistrationValidation.java
@@ -1,2 +1,55 @@
 package businesslogic;
-
+import java.util.regex.*;
+public class RegistrationValidation
+{
+	public boolean checkUserDetails(String email,String password,String confirmPassword)
+	{
+		    if(validPassword(password,confirmPassword) && validEmail(email))
+		    {
+		    	return true;
+		    }
+		    else
+		    	return false;
+	}
+	//password validation 
+	   private boolean validPassword(String password,String confirmPassword)
+	   {          String regex = "^(?=.*[0-9])"
+				+ "(?=.*[a-z])(?=.*[A-Z])"
+				+ "(?=.*[@#$%^&+=])"
+				+ "(?=\\S+$).{8,20}$";
+	     Pattern   p = Pattern.compile(regex);
+	     if( !password.equals(confirmPassword)|| password==null) {
+	    	 return false;
+	     }
+	     Matcher m =p.matcher(password);
+	     return m.matches();
+	   }
+	   ///Email validation
+	   private boolean validEmail(String email)
+	   {
+		   String a_z="abcdefghijklmnopqrstuvxyz";
+		    String numbers="0123456789";
+		    String specialChar="_-,@.,";
+		    if(email !=" ")
+		    {
+		    	 if(Check(email,a_z) && Check(email,numbers)&& Check(email,specialChar)) {
+					   return true;
+				}
+		    }
+		    return false;
+	   }
+	   private boolean Check(String i,String k)
+	   {
+		   for(int a=0;a<i.length();a++) 
+		   {
+			   for(int b=0;b<k.length();b++)
+			   {
+				   if(i.charAt(a)==k.charAt(b))
+				   {
+					   return true;
+				   }
+			   }
+		   }
+		   return false;
+	   }
+}
--- src/model/User.java
+++ src/model/User.java
@@ -1,1 +1,37 @@
 package model;
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+public class User{
+	private String email;
+	private String password;
+	public LocalDateTime date;
+	public User(String email ,String password,LocalDateTime date)
+	{
+		this.email=email;
+		this.password=password;
+		this.date=date;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}	
+	public LocalDateTime getDate() {
+		return date;
+	}
+}
